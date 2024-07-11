14ffb939605f7e15497d117c57fa0cc465546335
karniib <hadikarnib03@gmail.com>
2024-07-12 02:26:32 +0300

    "Added read, update, and delete methods to User model"

--- models/user.php
+++ models/user.php
@@ -14,4 +14,24 @@ class User
         $stmt->execute([$first_name, $last_name, $email, $password, $phone]);
         return ["message" => "user company created successfully"];
     }
+    public function read()
+    {
+        $stmt = $this->pdo->query('SELECT * FROM users');
+        return $stmt->fetchAll();
+    }
+
+    public function update($id, $first_name, $last_name, $email, $password, $phone)
+    {
+        $stmt = $this->pdo->prepare('UPDATE users SET id = ?, first_name = ?, last_name = ?, email = ? WHERE password = ?, phone = ?');
+
+        $stmt->execute([$id, $first_name, $last_name, $email, $password, $phone]);
+        return ["message" => "user updated successfully"];
+    }
+
+    public function delete($id)
+    {
+        $stmt = $this->pdo->prepare('DELETE FROM users WHERE id = ?');
+        $stmt->execute([$id]);
+        return ["message" => "user deleted successfully"];
+    }
 }
