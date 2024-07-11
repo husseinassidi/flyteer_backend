6300c79fa04bbe9eb20c27ad8526971173792878
husseinassidi <husseinassidi@gmail.com>
2024-07-12 02:39:13 +0300

    adding config

--- config/config.php
+++ config/config.php
@@ -1,27 +1,13 @@
 <?php
-// Database configuration
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'flight_management');
-define('DB_USER', 'root');
-define('DB_PASS', 'password');
+$servername = "localhost";
+$username = "root";
+$password = '';
+$db_name = 'flyteer';
 
-function getDBConnection() {
-    $conn = null;
-    try {
-        $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Connection error: " . $e->getMessage();
-    }
-    return $conn;
-}
+$conn = new mysqli($servername, $username, $password, $db_name);
 
-function testDBConnection() {
-    $conn = getDBConnection();
-    if ($conn) {
-        echo "Connection successful!";
-    } else {
-        echo "Connection failed!";
-    }
+if ($conn->connect_error) {
+    die('connection failed' . $conn->connect_error);
+} else {
+    echo "connection established";
 }
-?>
