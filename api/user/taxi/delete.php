52aaddeba6289811987e6fe9f480956000b396a9
Nibalab <abdalbakinibal@gmail.com>
2024-07-12 01:48:04 +0300

    adding delete.php file

---
+++ api/user/taxi/delete.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../../config/config.php';
+require_once '../../models/Taxi.php';
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+$taxi = new Taxi($pdo);
+
+$data = json_decode(file_get_contents("php://input"));
+
+if(isset($data->taxi_id)) {
+    $response = $taxi->delete($data->taxi_id);
+    echo json_encode($response);
+} else {
