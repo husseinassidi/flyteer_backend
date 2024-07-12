266330698e850a4e6f0509af68c85ec57bf842cd
Nibalab <abdalbakinibal@gmail.com>
2024-07-13 01:04:03 +0300

    fixing booking taxi api files

--- api/booking/taxi/read.php
+++ api/booking/taxi/read.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../../config/config.php';
-require_once '../../models/taxi_booking.php';
+require_once '../../../config/config.php';
+require_once '../../../models/taxi_booking.php';
 
 $pdo = getDBConnection();
 $taxiBooking = new TaxiBooking($pdo);
