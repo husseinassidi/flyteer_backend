fc96f8672607627a59494c8c66866c1d68286c2e
husseinassidi <husseinassidi@gmail.com>
2024-07-14 23:51:37 +0300

    reformatting the taxi api and database

--- api/taxi/create.php
+++ api/taxi/create.php
@@ -6,9 +6,16 @@ $pdo = getDBConnection();
 $taxi = new Taxi($pdo);
 
 $data = json_decode(file_get_contents("php://input"));
+// add license and driver
 
-if (isset($data->taxi_company_name) && isset($data->location) && isset($data->price_per_km) && isset($data->available_cars)) {
-    $response = $taxi->create($data->taxi_company_name, $data->location, $data->price_per_km, $data->available_cars);
+
+
+if (isset($data->taxi_company_name) && isset($data->location) && isset($data->price_per_km)&& isset($data->license) && isset($data->driver_name)) {
+    // $response = $taxi->create($data->taxi_company_name, $data->location, $data->price_per_km);
+
+    $response= $taxi->create($data-> taxi_company_name,$data->location ,$data->price_per_km ,$data->license ,$data->driver_name);
+
+
     echo json_encode($response);
 } else {
     echo json_encode(["error" => "Invalid input"]);
