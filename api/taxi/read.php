082ecadf58d0037b6214925e369210888604ae66
karniib <hadikarnib03@gmail.com>
2024-07-16 20:34:58 +0300

    fixed bugs

--- api/taxi/read.php
+++ api/taxi/read.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 require_once '../../config/config.php';
 require_once '../../models/taxi.php';
 
@@ -7,4 +17,3 @@ $taxi = new Taxi($pdo);
 
 $response = $taxi->read();
 echo json_encode($response);
-?>
