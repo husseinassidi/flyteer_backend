4e1077d64573e30015eaf6c130be801d7182651a
Nibalab <abdalbakinibal@gmail.com>
2024-07-16 18:36:11 +0300

    setting the taxi js files

---
+++ api/taxi/getTaxis.php
@@ -0,0 +1,16 @@
+<?php
+// Include configuration and model files
+require_once '../../config/config.php';
+require_once '../../models/taxi.php';
+
+// Get POST data
+$data = json_decode(file_get_contents("php://input"));
+
+// Assuming $pdo is defined and passed from the config.php file
+$taxiModel = new Taxi($pdo);
+
+// Example of retrieving a single user
+$response = $taxiModel->read();
+
+// Output response as JSON
+echo json_encode($response);
