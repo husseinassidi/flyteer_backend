c8deb329a0c545f8f7f69a3c105570b43fb7aafc
karniib <hadikarnib03@gmail.com>
2024-07-17 01:12:35 +0300

    fixed errors

--- api/taxi/update.php
+++ api/taxi/update.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 require_once '../../config/config.php';
 require_once '../../models/taxi.php';
 
@@ -7,10 +17,9 @@ $taxi = new Taxi($pdo);
 
 $data = json_decode(file_get_contents("php://input"));
 
-if (isset($data->taxi_id) && isset($data->taxi_company_name) && isset($data->location) && isset($data->price_per_km) && isset($data->color) && isset($data->type)) {
-    $response = $taxi->update($data->taxi_id, $data->taxi_company_name, $data->location, $data->price_per_km);
+if (isset($data->taxi_id) && isset($data->license) && isset($data->driver_name) && isset($data->taxi_company) && isset($data->location) && isset($data->price_per_km) && isset($data->color) && isset($data->type)) {
+    $response = $taxi->update($data->taxi_id, $data->taxi_company, $data->location, $data->price_per_km, $data->license, $data->driver_name, $data->color, $data->type);
     echo json_encode($response);
 } else {
     echo json_encode(["error" => "Invalid input"]);
 }
-?>
--- models/taxi.php
+++ models/taxi.php
@@ -1,47 +1,61 @@
 <?php
-class Taxi {
+class Taxi
+{
 
     // ($data-> taxi_company_name,$data->location ,$data->price_per_km ,$data->license ,$data->driver_name)
     private $pdo;
 
-    public function __construct($pdo) {
+    public function __construct($pdo)
+    {
         $this->pdo = $pdo;
     }
 
-    public function create($taxi_company_name, $location, $price_per_km, $license, $driver_name,$color,$type) {
+    public function create($taxi_company_name, $location, $price_per_km, $license, $driver_name, $color, $type)
+    {
         // Get taxi_company_id from taxi_company table
         $stmt = $this->pdo->prepare('SELECT taxi_company_id FROM taxi_company WHERE taxi_company_name = ?');
         $stmt->execute([$taxi_company_name]);
         $company = $stmt->fetch();
-    
+
         if (!$company) {
             return ["error" => "Taxi company does not exist."];
         }
-    
+
         $taxi_company_id = $company['taxi_company_id'];
-    
+
         // Insert into taxis using taxi_company_id, including license and driver_name
         $stmt = $this->pdo->prepare('INSERT INTO taxis (taxi_company, location, price_per_km, license, driver_name,color,type) VALUES (?, ?, ?, ?, ?, ?, ?)');
-        $stmt->execute([$taxi_company_id, $location, $price_per_km, $license, $driver_name,$color,$type]);
+        $stmt->execute([$taxi_company_id, $location, $price_per_km, $license, $driver_name, $color, $type]);
         return ["success" => "Taxi created"];
     }
-    
 
-    public function read() {
+
+    public function read()
+    {
         $stmt = $this->pdo->query('SELECT * FROM taxis');
         return $stmt->fetchAll();
     }
 
-    public function update($taxi_id, $taxi_company, $location, $price_per_km,$license,$driver_name,$color,$type) {
-        $stmt = $this->pdo->prepare('UPDATE taxis SET taxi_company = ?, location = ?, price_per_km = ?, license = ?, driver_name = ?, color=?, type = ?, WHERE taxi_id = ?');
+    public function update(
+        $taxi_id,
+        $taxi_company,
+        $location,
+        $price_per_km,
+        $license,
+        $driver_name,
+        $color,
+        $type
+    ) {
+        $stmt = $this->pdo->prepare('UPDATE taxis SET taxi_company = ?, location = ?, price_per_km = ?, license = ?, driver_name = ?, color = ?, type = ? WHERE taxi_id = ?');
         $stmt->execute([$taxi_company, $location, $price_per_km, $license, $driver_name, $color, $type, $taxi_id]);
         return ["success" => "Taxi updated"];
     }
 
-    public function delete($taxi_id) {
+
+    public function delete($taxi_id)
+    {
         $stmt = $this->pdo->prepare('DELETE FROM taxis WHERE taxi_id = ?');
         $stmt->execute([$taxi_id]);
         return ["success" => "Taxi deleted"];
     }
 }
-?>
