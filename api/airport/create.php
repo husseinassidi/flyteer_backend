23262c27aeaf59374f99a6a3fcc61ccf56854a84
karniib <hadikarnib03@gmail.com>
2024-07-17 21:28:43 +0300

    Added CORS headers and OPTIONS request handling to airport API endpoints

--- api/airport/create.php
+++ api/airport/create.php
@@ -1,5 +1,15 @@
 <?php
-// Include configuration file
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: DELETE, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once '../../config/config.php';
 
 // Get the PDO object
